f14ff387f94b65afcb47a388f73becb75d9bbbae
theo_sherry <sherr.theo@gmail.com>
2017-10-13 19:45:45 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="theopy3",
+    version="0.1",
+    url="https://github.com/theosherry/theopy3",
+    author="Theo Sherry",
+    author_email="sherr.theo@gmail.com",
+    license="MIT",
+    packages=["theopy3"],
+    zip_safe=False)
