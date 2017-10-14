a4ccd74ea6fe7be35308b7d46bed9fbbc491eecd
theo_sherry <sherr.theo@gmail.com>
2017-10-13 21:57:47 -0400

    upgrade get_user_confirmation to python3

--- theopy3/script_helpers/user_input.py
+++ theopy3/script_helpers/user_input.py
@@ -1,7 +1,7 @@
 import sys
 
 def get_user_confirmation(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
+    """Ask a yes/no question via input() and return their answer.
     
     "question" is a string that is presented to the user.
     "default" is the presumed answer if the user just hits <Enter>.
@@ -23,7 +23,7 @@ def get_user_confirmation(question, default="yes"):
 
     while 1:
         sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
+        choice = input().lower()
         if default is not None and choice == '':
             return default
         elif choice in valid.keys():
